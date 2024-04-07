f5161760c162755d74a2d291b85ee9fa12041355
Nruzhzhaugash <clonn262@gmail.com>
2024-04-08 03:54:45 +0500

    gdl,a

--- src/_pages/ApiPage/ui/ApiPage.tsx
+++ src/_pages/ApiPage/ui/ApiPage.tsx
@@ -7,7 +7,7 @@ export default function ApiPage() {
     <section className="pt-10">
       <h1 className="text-[32px] font-bold mb-5">API</h1>
       <p className="font-medium mb-5">{api("text")}</p>
-      <p className="mb-5">API: <a className="hover:underline mb-5" href="https://openweathermap.org/">https://openweathermap.org/</a></p>
+      <p className="mb-5">API: <a className="hover:underline mb-5" href="https://api.openweathermap.org/data/2.5/weather">https://api.openweathermap.org/data/2.5/weather</a></p>
       <p>{api("wha")}</p>
     </section>
   )
